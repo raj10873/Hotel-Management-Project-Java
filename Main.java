0f6e0473bbfbc6f791f55ba1f886fbcb5505792b
raj10873 <rajkumar@hamlyglobaltech.com>
2025-09-11 16:00:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+thhrt
 class Food implements Serializable
 {
     int itemno;
